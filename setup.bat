91e6c1ba651a24c4029410b8f076711945efbc3b
BHCrusher1 <blackholecruhser@gmail.com>
2019-06-09 10:18:11 +0900

    setup.batの改行コードをCRLFに & batの出力を修正

--- setup.bat
+++ setup.bat
@@ -1,10 +1,10 @@
 @echo off
 cd /d %~dp0
 
-pip install pixivpy >nul
-pip install beautifulsoup4 >nul
-pip install robobrowser >nul
-pip install lxml >nul
+python -m pip install pixivpy
+python -m pip install beautifulsoup4
+python -m pip install robobrowser
+python -m pip install lxml
 
 if not exist files\illustrator.csv (
     echo 11132499,小向 旭 > files\illustrator.csv
@@ -13,7 +13,7 @@ if not exist files\kyokou_log.txt (
     type nul > files\kyokou_log.txt
 )
 if not exist config.py (
-    echo current_dir:str =  > config.py
+    echo current_dir:str = "files\\" # By default to the "files" folder in the current directory. > config.py
     echo file_list_csv:str = "files\\file_list.txt" >> config.py
     echo illustrator_csv:str = "files\\illustrator.csv" >> config.py
     echo log_file:str = "files\\kyokou_log.txt" >> config.py
@@ -23,9 +23,9 @@ if not exist config.py (
     echo illustid_interval:float = 1.0 >> config.py
     echo get_illust_time:float = 2.0 >> config.py
     echo get_manga_page_time:float = 1.0 >> config.py
-    echo pixiv_id:str = >> config.py
-    echo password:str = >> config.py
-    echo user_id:str = >> conifg.py
+    echo pixiv_id:str = "" # pixiv_id is a user setting screen string. >> config.py
+    echo password:str = "" >> config.py
+    echo user_id:str = "" # user_id is a numeric value of personal url. >> config.py
 )
 
 echo sucsess!
